59543e842c1e716668bb330ba91c0c21fd61db06
mlum123 <michellelum123@gmail.com>
2022-02-08 11:44:27 -0800

    create script to write earnings call transcript metadata to CSV

---
+++ code/python/parsing_earnings_calls_metadata.py
@@ -0,0 +1,72 @@
+"""
+This script goes through all the parsed transcript JSON files for the years specified in
+transcript_years = ['2019', '2020', '2021'] below.
+
+It determines each transcript's filename, ticker (i.e. ULTA.OQ), company, quarter, half-year, and year.
+And writes that company metadata to a CSV file (/data/SCRIPTS/earnings_calls_scripts/earnings_calls_metadata.csv)
+where each line corresponds to one earnings call transcript for one company.
+
+To run this script:
+cd /data/SCRIPTS/earnings_calls_scripts
+python3 parsing_earnings_calls_metadata.py
+"""
+
+import os
+import json
+import csv
+
+def read_transcript_json_file(transcript_json_filename):
+    """ takes in the JSON file for a single earnings call transcript
+        returns a list of the filename, ticker, company, quarter, half_year, year for that transcript
+    """
+    f = open(transcript_json_filename) # open JSON file
+    transcript_json = json.load(f)     # load JSON into Python dictionary
+
+    # transcript_json["filename"] will look something like /data/TRANSCRIPTS_2019/2019-Feb-12-VNO.N-139861747920-transcript.txt
+    filename = transcript_json["filename"].split("/")[3]
+    ticker = filename.split("-")[3]
+    company = transcript_json["company"]
+    if transcript_json["quarter"]:
+        quarter = transcript_json["quarter"]
+    else:
+        quarter = ""
+
+    if transcript_json["half-year"]:
+        half_year = transcript_json["half-year"]
+    else:
+        half_year = ""
+    
+    if transcript_json["year"]:
+        year = transcript_json["year"]
+    else:
+        year = ""
+    return [filename, ticker, company, quarter, half_year, year]
+
+def write_list_of_lists_to_csv(list_of_lists, csv_path):
+    """ takes in a list of lists, where each inner list represents the metadata for one earnings call
+        and creates a csv file called at the path specified by csv_path with one line for each earnings call,
+        formatted as filename, ticker, company, quarter, half-year, year
+    """
+    with open(csv_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter = ',')
+        csvwriter.writerow(['Filename', 'Ticker', 'Company', 'Quarter', 'Half-Year', 'Year'])
+        for list in list_of_lists:
+            csvwriter.writerow(list)
+
+# to run script:
+# cd /data/SCRIPTS/earnings_calls_scripts
+# python3 parsing_earnings_calls_metadata.py
+if __name__ == "__main__":
+    # go through all parsed transcript JSON files in all 3 transcript folders (ex: /data/SCRIPTS/earnings_calls_scripts/parsed_transcripts/parsed_transcripts_2021)
+    # and save company metadata to transcript_metadata_lists (a list of lists)
+    transcript_metadata_lists = []
+    transcript_years = ['2019', '2020', '2021']
+    for transcript_year in transcript_years:
+        transcript_json_folder = '/data/SCRIPTS/earnings_calls_scripts/parsed_transcripts/parsed_transcripts_' + transcript_year
+        for transcript_json_filename in os.listdir(transcript_json_folder):
+            print(transcript_json_filename)
+            company_info = read_transcript_json_file(transcript_json_folder + '/' + transcript_json_filename)
+            transcript_metadata_lists.append(company_info)
+
+    # write transcript_metadata_lists list of lists to csv at /data/SCRIPTS/earnings_calls_scripts/earnings_calls_metadata.csv
+    write_list_of_lists_to_csv(transcript_metadata_lists, '/data/SCRIPTS/earnings_calls_scripts/earnings_calls_metadata.csv')
