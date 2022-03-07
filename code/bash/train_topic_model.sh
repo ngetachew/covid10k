5401efc3566ddafc90579c68ec089f0185de3219
mlum123 <michellelum123@gmail.com>
2022-03-06 17:21:32 -0800

    Add initial earnings calls topic model training bash script

---
+++ code/bash/train_topic_model.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Bash script for training topic model
+# Used for COVID earnings calls
+# Arguments: input Mallet .seq file from importing TSV file
+seqfile=$1
+
+bname=`basename $seqfile .seq`
+k=50    # num of topics
+
+source ~/.bashrc
+
+# outputprefix is output/seqfilename-numtopics-modelnum
+outputprefix=output/$bname-$k-0
+# Obtain initial model by training for 5000 iterations
+if [ ! -f $outputprefix.gz ]; then
+    iterations=500
+    echo train $outputprefix $seqfile
+    mallet train-topics --input $seqfile --num-topics $k --num-iterations $iterations --output-state $outputprefix.gz --output-topic-keys $outputprefix.keys --num-top-words 20 --diagnostics-file $outputprefix.diag.xml --evaluator-filename $outputprefix.evaluator --word-topic-counts-file $outputprefix.wordcounts.txt --topic-word-weights-file $outputprefix.wordweights.txt --output-doc-topics $outputprefix.doctopics &> $outputprefix.out
+fi
+
+# Obtain 9 more models, each trained by 100 more iterations than the last
+for i in {1..9}; do
+    prevoutputprefix=output/$bname-$k-$(($i-1)) # use previous model's output as input state to new model
+    if [ -f $prevoutputprefix.gz ]; then
+        outputprefix=output/$bname-$k-$i
+        iterations=100
+        echo train $outputprefix $seqfile
+        mallet train-topics --input $seqfile --num-topics $k --num-iterations $iterations --input-state $prevoutputprefix.gz --output-state $outputprefix.gz --output-topic-keys $outputprefix.keys --num-top-words 20 --diagnostics-file $outputprefix.diag.xml --evaluator-filename $outputprefix.evaluator --word-topic-counts-file $outputprefix.wordcounts.txt --topic-word-weights-file $outputprefix.wordweights.txt --output-doc-topics $outputprefix.doctopics &> $outputprefix.out
+    fi
+done
