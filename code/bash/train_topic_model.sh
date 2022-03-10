d570d48c63f2cef143414a0d0b20a06fb7c08959
mlum123 <michellelum123@gmail.com>
2022-03-09 23:14:05 -0800

    Update train topic model script to allow for topics of different sizes; and to write to txt files

--- code/bash/train_topic_model.sh
+++ code/bash/train_topic_model.sh
@@ -15,7 +15,7 @@ outputprefix=output/$bname-$k-0
 if [ ! -f $outputprefix.gz ]; then
     iterations=500
     echo train $outputprefix $seqfile
-    mallet train-topics --input $seqfile --num-topics $k --num-iterations $iterations --output-state $outputprefix.gz --output-topic-keys $outputprefix.keys --num-top-words 20 --diagnostics-file $outputprefix.diag.xml --evaluator-filename $outputprefix.evaluator --word-topic-counts-file $outputprefix.wordcounts.txt --topic-word-weights-file $outputprefix.wordweights.txt --output-doc-topics $outputprefix.doctopics &> $outputprefix.out
+    mallet train-topics --input $seqfile --num-topics $k --optimize-interval 10 --num-iterations $iterations --output-state $outputprefix.gz --output-topic-keys $outputprefix.keys.txt --num-top-words 20 --diagnostics-file $outputprefix.diag.xml --evaluator-filename $outputprefix.evaluator --word-topic-counts-file $outputprefix.wordcounts.txt --topic-word-weights-file $outputprefix.wordweights.txt --output-doc-topics $outputprefix.doctopics.txt &> $outputprefix.out
 fi
 
 # Obtain 9 more models, each trained by 100 more iterations than the last
@@ -25,6 +25,6 @@ for i in {1..9}; do
         outputprefix=output/$bname-$k-$i
         iterations=100
         echo train $outputprefix $seqfile
-        mallet train-topics --input $seqfile --num-topics $k --num-iterations $iterations --input-state $prevoutputprefix.gz --output-state $outputprefix.gz --output-topic-keys $outputprefix.keys --num-top-words 20 --diagnostics-file $outputprefix.diag.xml --evaluator-filename $outputprefix.evaluator --word-topic-counts-file $outputprefix.wordcounts.txt --topic-word-weights-file $outputprefix.wordweights.txt --output-doc-topics $outputprefix.doctopics &> $outputprefix.out
+        mallet train-topics --input $seqfile --num-topics $k --optimize-interval 10 --num-iterations $iterations --input-state $prevoutputprefix.gz --output-state $outputprefix.gz --output-topic-keys $outputprefix.keys.txt --num-top-words 20 --diagnostics-file $outputprefix.diag.xml --evaluator-filename $outputprefix.evaluator --word-topic-counts-file $outputprefix.wordcounts.txt --topic-word-weights-file $outputprefix.wordweights.txt --output-doc-topics $outputprefix.doctopics.txt &> $outputprefix.out
     fi
 done
